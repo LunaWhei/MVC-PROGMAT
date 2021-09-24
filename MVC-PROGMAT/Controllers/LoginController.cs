f83eb959a7fa0e393c16c133989f7c06e417c6dd
LunaWhei <osiadlo57@outlook.com>
2021-09-24 20:13:50 +0200

    Add project files.

---
+++ MVC-PROGMAT/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC_PROGMAT.Models;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace MVC_PROGMAT.Controllers
+{
+    public class LoginController : Controller
+    {
+        MySqlConnection cn;
+        MySqlDataAdapter da;
+        DataSet ds;
+
+        void DbConnect(string Login)
+        {
+            cn = new MySqlConnection("server=mysql03.kacperosiadlo.beep.pl;uid=mvcprogmat;pwd=Admin2020!;database=mvcprogmat");
+            
+            da = new MySqlDataAdapter("SELECT * FROM `Users` WHERE `Login` = '"+Login+"'", cn);
+            ds = new DataSet();
+            da.Fill(ds, "logins");
+            ViewData["string"] = ds.Tables["logins"];
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            DbConnect("Admin");
+            return View();
+            
+        }
+
+        [HttpPost]
+        public IActionResult VerifyUser(LoginModel user)
+        {
+            DbConnect(user.Login);
+            return View();
+        }
+    }
+}
---
+++ MVC-PROGMAT/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_PROGMAT.Models
+{
+    public class LoginModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+    }
+
+}
