d8b1cccf405e28150699079b85ddbdba35559a56
LunaWhei <osiadlo57@outlook.com>
2021-09-25 02:21:28 +0200

    Add project files.

---
+++ MVCProgmat/Controllers/LoginDataController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCProgmat.Models;
+using MySql.Data.MySqlClient;
+using System.Net;
+
+namespace MVCProgmat.Controllers
+{
+    public class LoginDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<LoginData> list1 = new List<LoginData>();
+            string mainconn = "Server=mysql03.kacperosiadlo.beep.pl;Uid=mvcprogmat;Pwd=Admin2020!;Database=mvcprogmat;Port=3306;SslMode=none";
+            MySqlConnection mysql = new MySqlConnection(mainconn);
+            string query = "select * from Users";
+            MySqlCommand comm = new MySqlCommand(query);
+            comm.Connection = mysql;
+
+            mysql.Open();
+            MySqlDataReader dr = comm.ExecuteReader();
+            while (dr.Read())
+            {
+                list1.Add(new LoginData
+                {
+                    Login = dr["Login"].ToString()
+                });
+            }
+            mysql.Close();
+            return View(list1);
+        }
+    }
+}
---
+++ MVCProgmat/Models/LoginData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProgmat.Models
+{
+    public class LoginData
+    {
+        public string Login { get; set; }
+    }
+}
